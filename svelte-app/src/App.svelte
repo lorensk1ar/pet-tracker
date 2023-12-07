072b23baaf1cdee3f8be292a6b2a2c97133c1aea
lorensk1ar <LorenSklar@outlook.com>
2023-12-07 14:01:09 -0500

    Create App.svelte

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,37 @@
+<script> 
+	import AddPet from './components/AddPet.svelte'
+	import ListPets from './components/ListPets.svelte'
+
+</script>
+
+<main>
+	<h1>Pet Tracker</h1>
+
+	<section>
+		<AddPet />
+	</section>
+
+	<section>
+		<ListPets />
+	</section>
+</main>
+
+
+<style>
+	main {
+		width: 360px;
+	}
+
+	h1 {
+		text-align: center;
+	}
+
+	section {
+		margin: 10px;
+		border: 1px solid black;
+		border-radius: 10px;
+		padding: 10px
+	}
+</style>
+
+
