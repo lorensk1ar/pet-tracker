c77a760d0128e20d6103c4a46acc45b00937f72c
lorensk1ar <LorenSklar@outlook.com>
2023-12-07 13:46:14 -0500

    Create api.py

---
+++ flask-app/src/api.py
@@ -0,0 +1,65 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import sqlite3
+
+app = Flask(__name__)
+CORS(app)
+db = 'pets.db'
+
+
+# INDEX
+@app.route('/')
+def hello():
+    return 'Hello, Pet Lover!'
+
+
+# CREATE
+@app.route('/api/pets', methods=['POST'])
+def create_pet():
+    try:
+        data = request.json
+        conn = sqlite3.connect(db)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO pets (name, picture, species, friendly) VALUES (?, ?, ?, ?)", 
+                       (data['name'], data['picture'], data['species'], data['friendly']))
+
+        conn.commit()
+        conn.close()
+        return jsonify({'message': ''}), 201
+    except Exception as e:
+        print(e)
+        return jsonify({'error': str(e)}), 500
+
+# READ ALL
+@app.route('/api/pets', methods=['GET'])
+def get_all_pets():
+    try:
+        conn = sqlite3.connect(db)
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM pets')
+        result = [{'id': row[0], 'name': row[1], 'picture': row[2], 'species': row[3], 'friendly': row[4]} for row in cursor.fetchall()]
+        conn.close()
+        return jsonify(result), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'error': str(e)}), 500
+
+# DELETE
+@app.route('/api/pets/<int:id>', methods=['DELETE'])
+def remove_pet(id):
+    sid = str(id)
+    try:
+        conn = sqlite3.connect(db)
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM pets WHERE id = ?', (sid))
+        conn.commit()
+        conn.close()
+        return jsonify({'message': 'Bye bye, #' + sid}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'error': str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
+
