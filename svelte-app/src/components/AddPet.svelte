d95ce432213d28e477e26f6d3c06956e6123a771
lorensk1ar <LorenSklar@outlook.com>
2023-12-08 09:36:39 -0500

    Update AddPet to fix species
    Fix AddPet so newPet species saves correctly to database

--- svelte-app/src/components/AddPet.svelte
+++ svelte-app/src/components/AddPet.svelte
@@ -1,5 +1,5 @@
 <script>
-  let pet = {
+  let newPet = {
     name: '',
     picture: 'Picture URL',
     species: '',
@@ -13,10 +13,10 @@
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(pet),
+        body: JSON.stringify(newPet),
       })
 
-      pet = {
+      newPet = {
         name: '',
         picture: 'Picture URL',
         species: '',
@@ -33,45 +33,47 @@
     }
   }
 
+  import pets from '../store/pets.js'
+
 </script>
 
 <form>
   <h3>New Pet</h3>
   <div>
     <label for="pet-name">Pet Name<span class="required">*</span>: </label>
-    <input type="text" id="pet-name" bind:value={pet.name} required/>
+    <input type="text" id="pet-name" bind:value={newPet.name} required/>
   </div><br>
 
   <div>
     <label for="profile-picture">Profile Picture<span class="required">*</span>: </label>
-    <input type="url" id="profile-picture" bind:value={pet.picture} required/>
+    <input type="url" id="profile-picture" bind:value={newPet.picture} required/>
   </div><br>
 
   <fieldset>
     <legend>Species<span class="required">*</span></legend>
     
     <label for="species-dog">
-      <input type="radio" name="species" id="species-dog" bind:value={pet.species} required>
+      <input type="radio" name="species" id="species-dog" bind:group={newPet.species} value="Dog" required>
       Dog
     </label>
 
     
     
     <label for="species-cat">
-      <input type="radio" name="species" id="species-cat" bind:value={pet.species} required>
+      <input type="radio" name="species" id="species-cat" bind:group={newPet.species} value="Cat" required>
       Cat
     </label>
 
     <label for="species-fish">
-      <input type="radio" name="species" id="species-fish" bind:value={pet.species} required>
+      <input type="radio" name="species" id="species-fish" bind:group={newPet.species} value="Fish" required>
       Fish
     </label>
   </fieldset><br>
 
   <div>
     <label for="pet-friendly">
-      Is {pet.name} friendly?
-      <input type="checkbox" id="pet-friendly"  bind:value={pet.friendly}/>
+      Is {newPet.name} friendly?
+      <input type="checkbox" id="pet-friendly"  bind:value={newPet.friendly}/>
     </label>
   </div><br>
 
