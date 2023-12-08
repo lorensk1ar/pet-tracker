533d72b3b2dd52549ad708991ad40b98d0895264
lorensk1ar <LorenSklar@outlook.com>
2023-12-08 12:41:09 -0500

    Reset pets.js
    Clear previous work and establish a store/pets.js to maintain state for pets

--- svelte-app/src/store/pets.js
+++ svelte-app/src/store/pets.js
@@ -1,45 +1,5 @@
-/*
-let posts = [];
+import {writable} from 'svelte/store'
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json())
-  .then(data => posts = data);
+let pets = writable([])
 
-*/
-
-import { writable } from 'svelte/store'
-const pets = writable([
-  {
-    "name": "Fluffy",
-    "picture": "fluffy.png",
-    "species": "Dog",
-    "friendly": true
-  },
-  {
-    "name": "Whiskers",
-    "picture": "whiskers.png",
-    "species": "Cat",
-    "friendly": false
-  },
-  {
-    "name": "Bubbles",
-    "picture": "bubbles.png",
-    "species": "Fish",
-    "friendly": false
-  }
-])
-
-const petList = {
-  subscribe: pets.subscribe,
-  addPet(newPet) {
-    pets.update(items => {
-      return {...items, newPet}
-    })
-  removePet(petId) {
-    pets.update(items => {
-      return {...items}
-    })
-  }
-}
-
-export default petList
+export default pets
